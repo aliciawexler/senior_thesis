6bcfe03e6b8d2cb34939ec684edbc8d486e7f5f9
aliciawexler <aliciarwexler@gmail.com>
2026-02-02 13:25:27 -0500

    Refactor generate_yaml.py for clarity and functionality
    This file takes a csv file of sequences and turns each into a yaml file in a new directory that is in the same directory as the csv file. It generates an all-alanine sequence that is 2 residues longer than the actual sequence for terminal NHE and ACE caps.

--- generate_yaml.py
+++ generate_yaml.py
@@ -1,4 +1,7 @@
 '''
+This file takes a csv file of sequences and turns each into a yaml file in a new directory that's in the same directory as the csv file. This file turns your sequence into an all-alanine sequence that is 2 residues
+longer than the actual sequence. This is for the addition of terminal NHE and ACE caps. Each yaml file is named "sequence_<csv_line_number>.yaml".
+
 Setup of yaml file:
 
 Version: <csv line #>
@@ -12,9 +15,12 @@ sequences:
         - position: <position number>
           ccd: <'B3A' for 4 and '0W6' for 5>
         -<other mods>
+EDITS:
+Line 28: Add the non-canonical amino acid's CCD code to the mod_map if not already there. Make sure the character matches what's in your sequences.
+Line 33: Change <OUTPUT> to the desired output directory name
+Line 70: Change <CSV> to the input csv name
 '''
-#To Run: In line 27, change output_dir folder to desired folder name.
-#In line 64, change csv file to desired file name
+
 import csv
 import yaml
 import os
@@ -24,7 +30,7 @@ mod_map = {
     'D': 'DAL'
 }
 
-def convert_line_to_yaml(line, index, output_dir="yaml_output_10mer_abd_127"):
+def convert_line_to_yaml(line, index, output_dir="<OUTPUT>"):
     sequence_alone = ''.join(['A' for _ in line.strip()])  # Replace each number with 'A', add 2 additional As to the sequence
     numResidues = len(line)
     sequence = sequence_alone +"AA"
@@ -61,5 +67,6 @@ def process_csv(file_path):
             line = ''.join(row)  # Flatten row into a string
             convert_line_to_yaml(line, index)
 
-process_csv("abd_10mers1.csv")
+process_csv("<CSV>.csv")
 print("Done!")
+
