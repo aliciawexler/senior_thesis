b77c0a787179e225a5010a1807552089075e6666
aliciawexler <aliciarwexler@gmail.com>
2026-02-02 11:21:45 -0500

    Modify gen_seq.py for dynamic CSV output and comments
    Updated the script to allow dynamic CSV output file naming and added comments for configuration.

--- gen_seq.py
+++ gen_seq.py
@@ -1,9 +1,19 @@
+'''
+This file generates all possible sequences of a specified length of the amino acids represented by their one-letter codes in "ALPHABET". Optionally, by setting "WINDOW" to an integer less than "LENGTH", 
+you may also omit sequences containing repeated motifs as long as that window, such that for LENGTH=3 and WINDOW=2, if "ABD" has already been generated, "DAB" will be ommitted due to its repeated "AB" motif.
+All sequences are saved to a csv file of your specification.
+EDITS:
+Line 15: Edit the one-letter codes in ALPHABET to include all of the amino acids and only the amino acids you'd like to generate sequences of.
+Line 16: Set LINEAR to your desired sequence length
+Line 17: Set WINDOW to your desired linear motif length (or set to the same integer as LENGTH if you'd rather not use the WINDOW feature)
+Line 29: Set <CSV> to the name of your desired csv output file
+'''
 #!/usr/bin/env python3
 import csv
 from itertools import product
 
 ALPHABET = ("D", "A")
-LENGTH = 10
+LENGTH = 10  #length of sequences
 WINDOW = 10  # subsequence length to enforce uniqueness
 
 
@@ -16,7 +26,7 @@ def main() -> None:
     seen_xmers = set()
     kept = 0
 
-    with open("abd_test10.csv", "w", newline="", encoding="utf-8") as f:
+    with open("<CSV>.csv", "w", newline="", encoding="utf-8") as f:
         w = csv.writer(f)
 
         # Deterministic order: lexicographic by ALPHABET order (D, A, B)
